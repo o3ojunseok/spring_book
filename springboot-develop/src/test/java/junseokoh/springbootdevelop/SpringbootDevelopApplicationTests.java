d9bc6182259ea79f1de46ce8897dafb842876e3d
o3ojunseok <o3ojunseok@gmail.com>
2023-06-06 00:15:50 +0900

    setting

---
+++ springboot-develop/src/test/java/junseokoh/springbootdevelop/SpringbootDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package junseokoh.springbootdevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDevelopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
