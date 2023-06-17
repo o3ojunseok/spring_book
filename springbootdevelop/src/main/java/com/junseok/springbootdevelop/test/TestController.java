7cfea00ad052e80bc3a34b93e8e75ca673e809c7
o3ojunseok <o3ojunseok@gmail.com>
2023-06-17 18:06:25 +0900

    3장 java 17

--- springbootdevelop/src/main/java/com/junseok/springbootdevelop/test/TestController.java
+++ springbootdevelop/src/main/java/com/junseok/springbootdevelop/test/TestController.java
@@ -1,10 +1,14 @@
-package com.junseok.springbootdevelop;
+package com.junseok.springbootdevelop.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Autowired
+    TestService testService;
 
     @GetMapping("/test")
     public String test() {
