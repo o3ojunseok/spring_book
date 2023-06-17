edabf93e93d66e84413a98bf62729730216a006e
o3ojunseok <o3ojunseok@gmail.com>
2023-06-17 17:39:55 +0900

    book

---
+++ springbootdevelop/src/main/java/com/junseok/springbootdevelop/SpringbootdevelopApplication.java
@@ -0,0 +1,13 @@
+package com.junseok.springbootdevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootdevelopApplication.class, args);
+    }
+
+}
---
+++ springbootdevelop/src/test/java/com/junseok/springbootdevelop/SpringbootdevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junseok.springbootdevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdevelopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
