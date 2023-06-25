12e403d3b75734683e3e919d4f44d2028f7ffabf
o3ojunseok <o3ojunseok@gmail.com>
2023-06-26 00:26:01 +0900

    Jwt Token Provider

---
+++ springbootdevelop/src/main/java/com/junseok/springbootdevelop/domain/RefreshToken.java
@@ -0,0 +1,17 @@
+package com.junseok.springbootdevelop.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+}
