4b8a6bc45986570eab475741c779eeb73ca0f18a
o3ojunseok <o3ojunseok@gmail.com>
2023-06-26 01:01:54 +0900

    RefreshToken

---
+++ springbootdevelop/src/main/java/com/junseok/springbootdevelop/controller/TokenApiController.java
@@ -0,0 +1,25 @@
+package com.junseok.springbootdevelop.controller;
+
+import com.junseok.springbootdevelop.dto.CreateAccessTokenRequest;
+import com.junseok.springbootdevelop.dto.CreateAccessTokenResponse;
+import com.junseok.springbootdevelop.service.TokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class TokenApiController {
+    private final TokenService tokenService;
+
+    @PostMapping("/api/token")
+    public ResponseEntity<CreateAccessTokenResponse> createNewAccessToken(@RequestBody CreateAccessTokenRequest request) {
+        String newAccessToken = tokenService.createNewAccessToken(request.getRefreshToken());
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new CreateAccessTokenResponse(newAccessToken));
+    }
+}
---
+++ springbootdevelop/src/main/java/com/junseok/springbootdevelop/service/RefreshTokenService.java
@@ -0,0 +1,17 @@
+package com.junseok.springbootdevelop.service;
+
+import com.junseok.springbootdevelop.domain.RefreshToken;
+import com.junseok.springbootdevelop.repository.RefreshTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RefreshTokenService {
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken findByRefreshToken(String refreshToken) {
+        return refreshTokenRepository.findByRefreshToken(refreshToken)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected token"));
+    }
+}
