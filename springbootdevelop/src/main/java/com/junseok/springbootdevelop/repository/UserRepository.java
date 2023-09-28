aeaff543d2b12e39dbe62a3201ed4b1cc63f2860
o3ojunseok <o3ojunseok@gmail.com>
2023-09-28 23:39:15 +0900

    test

--- springbootdevelop/src/main/java/com/junseok/springbootdevelop/repository/UserRepository.java
+++ springbootdevelop/src/main/java/com/junseok/springbootdevelop/repository/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email); // email로 사용자 정보 가져오기
+    Optional<User> findByEmail(String email);
 }
